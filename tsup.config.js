c66c461787fbe46a59cc5905c7f42b1e93a434c8
Ryan Long <rlong@redhat.com>
2025-01-16 17:26:28 -0500

    fix: remove comments

--- tsup.config.js
+++ tsup.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],  // Updated path to src directory
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm', 'iife'],
   dts: true,
   clean: true,
